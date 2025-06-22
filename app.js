568165ba834f804a6f9e2300802489aaca5f6ffb
Khizra Adnan <adnankhizra39@gmail.com>
2025-06-22 22:26:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// 1
+let username = "Khizra Adnan";
+console.log(username);
+// 2
+let myName = "Khizra Adnan";
+console.log(myName);
+// 3
+let message;
+message = "Hello World";
+alert(message);
+// 4
+let studentName = "Khizra Adnan";
+let age = 19;
+let grade = "12th Grade";
+let schoolName = "The Educators School";
+let address = "123 Main Street, Karachi";
+alert("Name: " + studentName);
+alert("Age: " + age);
+alert("Grade: " + grade);
+alert("School: " + schoolName);
+alert("Address: " + address);
+// 5
+let script = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(script);
+// 6
+let email = "adnankhizra@gmail.com";
+alert("My email address is " + email);
+// 7
+let book = "A smarter way to learn JavaScript";
+alert("I am reading a book titled " + book);
+// 8
+let display = "YAY! I can write HTML content through JavaScript"
+document.writeln(display);
+// 9
+let decorativeLine = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(decorativeLine);
+document.writeIn(decorativeLine);
+
+
+
+
+
+
